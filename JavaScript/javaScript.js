13cde159ac995dd98d55a2e65acd74a3327aea7d
RAQEEB212 <saneelmemon45@gmail.com>
2023-06-22 18:26:50 +0500

    day # 3.work on js and some sort of advance css

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,93 @@
+// let  clickbutton = document.querySelector('button')
+// clickbutton.addEventListener("click", submittionmessage)
+//
+// function submittionmessage(){
+//     alert('submitted')
+// }
+
+
+
+// let  clickbutton = document.querySelector("button")
+// clickbutton.addEventListener("click", inputname)
+//
+// function inputname(){
+//     clickbutton.textContent = prompt("enter the data");
+// }
+
+
+
+//
+// let b;
+// console.log(typeof b)
+
+
+// let time = prompt("what's time");
+//
+// if (time>5){
+//     alert("good morning")
+// }
+// else{
+//     alert("good evening")
+// }
+
+
+
+// let condition = prompt("enter the condition")
+//
+// switch (condition){
+//     case "1" :
+//         alert("this is 1");
+//         break;
+//     case "2" :
+//         alert("this is 2");
+//         break;
+//     case "3" :
+//         alert("this is 3")
+//         break;
+//     default :
+//         alert("this is default")
+//         break;
+// }
+
+
+// for (let i=0;i<=11;i++){
+//      console.log("this is 10 loop")
+//  }
+
+
+//for in loop
+
+// let family = {
+//
+//     child : "ahmed",
+//     age : 22
+// }
+//
+// for(let key in family){
+//     console.log(key , family[key]);
+//}
+
+
+
+// let names = ["ahmed", "lutuf" , "memon"]
+//
+// for(let index in names){
+//     console.log(index,names[index])
+// }
+
+// let names = ["ahmed", "lutuf" , "memon"]
+//
+// let of;
+// for (let nam in of names){
+//     console.log(nam)
+// }
+
+
+function  cookmeggi(megi,water,tapali){
+    alert("you meggi will  be ready in "+megi+" with using  water "+ water +" with tapali "+ tapali+" your megi will be ready " );
+
+}
+cookmeggi(1,1,1)
+
+
+
