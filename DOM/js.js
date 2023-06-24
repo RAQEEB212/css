5705d0bc6265b0ce7a0032127b62bd07132450ca
RAQEEB212 <saneelmemon45@gmail.com>
2023-06-24 12:55:16 +0500

    day # 3.work on js and some sort of advance css

---
+++ DOM/js.js
@@ -0,0 +1,47 @@
+// window.onload = function(){
+//
+// }
+
+console.log('working on DOM')
+// console.log(document.documentElement)
+// console.log(document.body)
+// console.log(document.head)
+// console.log(document.body.childNodes)
+// console.log(document.body.children)
+//
+// console.log(document.body.firstElementChild)
+// console.log(document.body.lastElementChild)
+
+// for (node of document.body.children){
+//  console.log(node)
+// }
+
+
+// const elementOfbody =  Array.from(document.body.children)
+// console.log(elementOfbody)
+//
+// const ultag = document.body.children[0]
+// const firstil = ultag.children[0]
+// const secondil = firstil.nextElementSibling
+// const previousli = secondil.previousElementSibling.textContent
+// console.log(previousli)
+
+// console.log(previousli)
+// console.log(ultag.nextElementSibling)
+
+// const table = document.body.children[1]
+// // console.log(table.tBodies[0].rows[0].cells[0].style = "background-color:green")
+// console.log(table.tBodies[0].rows[0].style = "background-color:green")
+// console.log(table.tBodies[0].rows[1].style = "background-color:blue")
+// console.log(table.tBodies[0].rows[2].style = "background-color:pink")
+// console.log(table.tBodies[0].rows[3].style = "background-color:white")
+// console.log(table.tBodies[0].rows[4].style = "background-color:red")
+//
+// console.log(table.tBodies[0].style = "overflow-x:auto;")
+// console.log(table.tBodies[0].style = "display:grid")
+
+console.log(tableforjs.style.background = "red")
+const row = document.getElementById("column")
+row.style.backgroundColor = "white";
+
+
